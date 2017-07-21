8c1b814ece5faf1a3c3f559e98529066ae47899a
Liran Cohen <liran.cohen@primarydata.com>
2017-07-22 02:22:14 +0300

    add logs and http code response for all response

---
+++ src/main/java/com/utils/SingleLogger.java
@@ -0,0 +1,8 @@
+package com.utils;
+import java.util.logging.Logger;
+/**
+ * Created by liran on 21/07/17.
+ */
+public class SingleLogger {
+    public final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+}
