dd54b6f192c99ef2d9212f45a4f78a672ab317fd
Liran Cohen <liran.cohen@primarydata.com>
2017-08-26 20:40:23 +0300

    fetch all update by  patient ID in HQL query

--- src/main/java/com/dao/PatientModel.java
+++ src/main/java/com/dao/PatientModel.java
@@ -1,12 +1,14 @@
 package com.dao;
 
 
-import com.dm.Activity;
-import com.dm.Patient;
+import com.dm.*;
+import com.dm.dbResult.ActivityDBResult;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -29,26 +31,46 @@ public class PatientModel{
         return patientModelInstance;
     }
 
-    public String getAllUpdatesByPatientID(String i_PatientID)throws HibernateException{
+    public List<PatientRecord> getAllUpdatesByPatientID(String i_PatientID)throws HibernateException{
         Session session = null;
         try {
             session = modelGenerics.getSessionFactory().openSession();
             Transaction transaction = session.beginTransaction();
-
-            Patient patient = session.get(Patient.class, i_PatientID);
-            List<Activity> listOfActivityName = session.createNativeQuery(format("select ACTIVITY_NAME from PATIENT_RECORD,PATIENT_RECORD_ACTIVITY where PATIENT_RECORD_ACTIVITY.PATIENT_RECORDS_ID=PATIENT_RECORD.PATIENT_RECORDS_ID and PATIENT_RECORD.PATIENT_ID=%s", i_PatientID)).list();
-
-            transaction.commit();//TODO add list of all options
-
-            StringBuilder buildPatientFile = new StringBuilder();
-            buildPatientFile.append(format("***********************************************Start-Report-FOR-The-Following-Patient ID: %s ***********************************************\n",i_PatientID));
-            buildPatientFile.append(format("%s\n",patient.toString()));
-            buildPatientFile.append(format("%s\n",getObjectListAsJsonList(listOfActivityName)));
-            buildPatientFile.append(format("***********************************************End-Report-FOR-The-Following-Patient ID: %s *************************************************\n",i_PatientID));
-            return buildPatientFile.toString();
+            List<PatientRecord> patientRecords = session.createQuery(format("select distinct p from PATIENT_RECORD as p join p.listOfActivityUpdate with p.patientID=%s",i_PatientID)).list();
+            transaction.commit();
+            return patientRecords;
         } finally {
             if(session!=null)
                 session.close();
         }
+    }
+//    private String buildReport(List<PatientRecord> i_PatientRecords){
+//        StringBuilder buildPatientFile = new StringBuilder();
+//        buildPatientFile.append(format("***********************************************Start-Report-FOR-The-Following-Patient ID: %s ***********************************************\n",i_PatientID));
+//        buildPatientFile.append(format("%s\n",i_PatientRecords.toString()));
+//        buildPatientFile.append(format("%s\n",getObjectListAsJsonList(i_PatientRecords)));
+//        buildPatientFile.append(format("***********************************************End-Report-FOR-The-Following-Patient ID: %s *************************************************\n",i_PatientID));
+//        return buildPatientFile.toString();
+//    }
+
+    public static void main(String args[]){
+        PatientModel patientModel = new PatientModel();
+//        patientModel.getAllUpdatesByPatientID("1");
+//        Link link = new Link();
+//        link.setLinkHeadLine("news");
+//        link.setLinkURL("url of news");
+//        Link link2 = new Link();
+//        link2.setLinkHeadLine("news2");
+//        link2.setLinkURL("url of news2");
+//        ModelGenerics.getModelGenericsInstance().addObjectToDB(link);
+//        ModelGenerics.getModelGenericsInstance().addObjectToDB(link2);
+        //patientModel.getAllUpdatesByPatientID("1")
+        List<PatientRecord> list =patientModel.getAllUpdatesByPatientID("1");
+        for(PatientRecord l:list){
+            for(ActivityUpdate w:l.getListOfActivityUpdate()){
+                System.out.println(w.getActivityName());
+            }
+        }
+
     }
 }
