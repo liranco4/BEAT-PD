73ac435dddaa9f712881548a5bdd60b04bd35ba8
Liran Cohen <liran.cohen@vatbox.com>
2017-10-01 23:21:43 +0300

    update 2

--- src/main/java/com/beatpd/controller/LoginController.java
+++ src/main/java/com/beatpd/controller/LoginController.java
@@ -6,10 +6,7 @@ import org.hibernate.HibernateException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Level;
 
@@ -24,20 +21,25 @@ import static java.lang.String.format;
 public class LoginController {
 
     private UserModel userModel = UserModel.getUserModelInstance();
+
     @RequestMapping(value = "/Login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
     @ResponseBody
     public ResponseEntity loginUser(@RequestBody User user) {
         try {
             if (userModel.checkCredentials(user))
                 return ResponseEntity.ok("{success: user logged-in}");
-            return  ResponseEntity.status(HttpStatus.FORBIDDEN).body("{error:not found}");
-        } catch(HibernateException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("{error:not found}");
+        } catch (HibernateException e) {
             LOGGER.log(Level.INFO, format("error in login: %s", e.getStackTrace().toString()));
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(format("{error:%s}", e.getMessage()));
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             LOGGER.log(Level.INFO, format("error in login: %s", e.getStackTrace().toString()));
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(format("{error:%s}", e.getMessage()));
         }
+    }
+
+    @RequestMapping(value = "/Login", method = RequestMethod.GET)
+    public String Login() {
+        return "redirect:/pages/LoginPage.html";
     }
 }
