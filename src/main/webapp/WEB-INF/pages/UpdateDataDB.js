4e8d98e222e946f15f4313746a160b8d6a77bcee
Liran Cohen <liran.cohen@primarydata.com>
2017-10-21 10:15:15 +0300

    add update activity to project

--- src/main/webapp/WEB-INF/pages/UpdateDataDB.js
+++ src/main/webapp/WEB-INF/pages/UpdateDataDB.js
@@ -3,22 +3,12 @@ function UpdateItemsToDB(rowId)
 
  if($("#ActivitiesTab")[0].parentElement.classList[0] == "active")
  {
-
-         var ActivityNameToUpdate =  rowId.parentNode.parentNode.cells[2].innerText;
-  $("#txtUpdateName").val(ActivityNameToUpdate);
-
-
-   // ddlSubMenu.append(rowId.parentNode.parentNode.cells[3].innerHTML);
-  $("#ddlUpdateSubMenu").append(rowId.parentNode.parentNode.childNodes[3].children[0]);
-
-
-        // $("#ddlMainGeneral option").each(function () {
-         //$("#ddlUpdateSubMenu").push($(this).text());
-         //});
-
-
+    $("#txtUpdateName").prop("readonly", true);
+    var ActivityNameToUpdate =  rowId.parentNode.parentNode.cells[2].innerText;
+    $("#txtUpdateName").val(ActivityNameToUpdate);
+    $("#ddlUpdateSubMenu").append( rowId.parentNode.parentNode.childNodes[3].children[0].options );
     document.getElementById("ModalUpdateBody").style.display='block';
-     UpdateModal.style.display = "block";
+    UpdateModal.style.display = "block";
 
   // UpdateActivityToDB(rowId);
  }
@@ -59,36 +49,33 @@ function UpdateItemsToDB(rowId)
 //update Activity
 function UpdateActivityToDB(rowId)
 {
+   //var RowToUpdate = rowId;
+   var acToUpdate= new Activity();
+   acToUpdate.activityName = $("#txtUpdateName").val();
 
-              var RowToDelete = rowId;
-              var acToUpdate= new Activity();
-              acToUpdate.activityName = RowToDelete.parentNode.parentNode.cells[2].innerText;
-
-
-         var SubMenuArr = [];
-        $("#ddlSubMenu option").each(function () {
-        SubMenuArr.push($(this).text());
-        });
+   var SubMenuArr = [];
+   $("#ddlUpdateSubMenu option").each(function () {
+   SubMenuArr.push($(this).text());
+   });
 
    var sm = [new subMenu()];
-   for(var i = 0; i< SubMenuArr.length ;i++ )
-  {
+   for(var i = 0; i< SubMenuArr.length ;i++)
+   {
     sm[i] = new subMenu();
     sm[i].subMenu = SubMenuArr[i];
    }
 
-             acToUpdate.subMenus = sm;
-
-              var myJSON = JSON.stringify(acToUpdate);
-
+      acToUpdate.subMenus = sm;
+      var myJSON = JSON.stringify(acToUpdate);
       $.ajax({
                   url: "http://localhost:8080/BEAT-PD/Admin/Update/Activity",
                   cache: false,
-                  type: "DELETE",
+                  type: "PUT",
                   data: myJSON,
                   contentType: "application/json;charset=utf-8",
-                  complete: function() {
-            }
+                  complete: function(data) {
+                     if(data.status != 200 ) alert('Error in updating data to DB:' + data);
+                   }
               });
 
               location.reload();
